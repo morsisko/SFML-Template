99c987bea2243e4d7e913dc361c0e1b5b54fccc6
morsisko <morsisko@gmail.com>
2017-09-03 21:55:20 +0200

    minimal working example of states and splashscreen

---
+++ SFML-Template/SplashScreen.cpp
@@ -0,0 +1,29 @@
+#include "SplashScreen.h"
+
+
+
+SplashScreen::SplashScreen(GameStateManager* manager, sf::RenderWindow* window) : State(manager, window)
+{
+	splashTexture.loadFromFile("assets/logo.png");
+	splashSprite.setTexture(splashTexture);
+
+	splashSprite.setPosition(window->getSize().x / 2 - splashTexture.getSize().x / 2, window->getSize().y / 2 - splashTexture.getSize().y / 2);
+}
+
+void SplashScreen::handleEvent(const sf::Event & event)
+{
+}
+
+void SplashScreen::render()
+{
+	window->draw(splashSprite);
+}
+
+void SplashScreen::update(float deltaTime)
+{
+}
+
+
+SplashScreen::~SplashScreen()
+{
+}
---
+++ SFML-Template/SplashScreen.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "State.h"
+class SplashScreen :
+	public State
+{
+private:
+	sf::Texture splashTexture;
+	sf::Sprite splashSprite;
+public:
+	SplashScreen(GameStateManager* manager, sf::RenderWindow* window);
+	virtual void handleEvent(const sf::Event &event);
+	virtual void render();
+	virtual void update(float deltaTime);
+	virtual ~SplashScreen();
+};
+
